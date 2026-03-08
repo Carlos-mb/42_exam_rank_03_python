022a5f513f6ed889636caf671518561e2d9dbc86
Carlos <39302320+Carlos-mb@users.noreply.github.com>
2026-03-08 19:51:25 +0100

    Add problem solution for Valid Parentheses

---
+++ Valid_Parentheses.py
@@ -0,0 +1,30 @@
+"""You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Return true if s is a valid string, and false otherwise."""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        par = 0
+        ll = 0
+        cor = 0
+        for l in s:
+            if l == "(":
+               par += 1
+            elif l == ")":
+                par -= 1
+            elif l == "{":
+                ll += 1
+            elif l == "}":
+                ll -= 1
+            elif l == "[":
+                cor += 1
+            elif l == "]":
+                cor -= 1
+            if par * ll * cor < 0:
+                return False
+        return True
